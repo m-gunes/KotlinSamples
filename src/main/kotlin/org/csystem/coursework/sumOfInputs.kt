dbad6ce0ca71a746c5d9cdddb981dabb150a8ea5
m-gunes <mistafagunes@gmail.com>
2024-09-02 15:51:15 +0200

    coursework sum of inputs

---
+++ src/main/kotlin/org/csystem/coursework/sumOfInputs.kt
@@ -0,0 +1,22 @@
+package org.csystem.coursework
+
+
+/*---------------------------------------------------------------------------------------------------------------------
+Sınıf Çalışması: Klavyeden sıfır girilene kadar alınan sayıların toplamını bulan programı yazınız.
+Örnekte break deyimini kullanabilirsiniz.
+---------------------------------------------------------------------------------------------------------------------*/
+fun main() = sumOfInputs()
+
+
+fun sumOfInputs() {
+    println("Let's start enter a numbers:")
+    var total = 0
+
+    while (true) {
+        val a = readln().toInt()
+        if(a == 0)
+            break
+        total += a
+    }
+    println("total = $total")
+}
