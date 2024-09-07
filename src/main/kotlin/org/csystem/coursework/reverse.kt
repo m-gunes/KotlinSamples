163b81f0f40f962f6e85e99bb6475b20419a0bb3
m-gunes <mistafagunes@gmail.com>
2024-09-07 21:36:57 +0200

    Create reverse.kt

---
+++ src/main/kotlin/org/csystem/coursework/reverse.kt
@@ -0,0 +1,32 @@
+package org.csystem.coursework
+
+/*---------------------------------------------------------------------------------------------------------------------
+    Sınıf Çalışması:
+        Parametresi ile aldığı Int türden bir sayının tersini döndüren reverse isimli fonksiyonu yazınız ve aşağıdaki
+        kod ile test ediniz.
+
+        Algoritma: 123 -> 3 -> 3 * 10 + 2 = 32 -> 32 * 10 + 1 = 321
+---------------------------------------------------------------------------------------------------------------------*/
+fun main() = runReverseTest()
+
+fun runReverseTest() {
+    while (true) {
+        print("Input a number: ")
+        val value = readln().toInt()
+        if(value == 0)
+            break
+
+        println("Reverse of $value is ${reverse(value)}")
+    }
+}
+
+fun reverse(a: Int): Int {
+    var temp = a
+    var result = 0
+
+    while(temp != 0) {
+        result = result * 10 + temp % 10
+        temp /= 10
+    }
+    return result
+}
