108db5dfb422075b5d4945814a6d3a46ec5cee70
m-gunes <mistafagunes@gmail.com>
2024-09-07 21:37:01 +0200

    Create isPalindrome.kt

---
+++ src/main/kotlin/org/csystem/coursework/isPalindrome.kt
@@ -0,0 +1,23 @@
+package org.csystem.coursework
+
+/*---------------------------------------------------------------------------------------------------------------------
+    Sınıf Çalışması:
+        Parametresi ile aldığı Int türden bir sayının palindrom olup olmadığını test eden isPalindrome fonksiyonunu
+        yazınız ve aşağıdaki kod ile test ediniz.
+---------------------------------------------------------------------------------------------------------------------*/
+
+fun main() = runIsPalindromeTest()
+
+fun runIsPalindromeTest() {
+    while (true) {
+        print("Input a number: ")
+        val value = readln().toInt()
+        println("$value is ${if(isPalidrome(value)) "palindrome" else "not palidrome"}")
+
+        if(value == 0)
+            break
+    }
+
+}
+
+fun isPalidrome(a: Int) = reverse(a) == a
