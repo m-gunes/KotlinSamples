d661aa3d74f648aebf52e049bb4df94272aaa79c
m-gunes <mistafagunes@gmail.com>
2024-09-13 09:13:08 +0200

    Create isArmstrong.kt

---
+++ src/main/kotlin/org/csystem/coursework/isArmstrong.kt
@@ -0,0 +1,53 @@
+package org.csystem.coursework
+
+import kotlin.math.log10
+
+/*---------------------------------------------------------------------------------------------------------------------
+    Sınıf Çalışması: Parametresi ile aldığı bir sayının basamaklarının basamak sayıncı kuvvetleri toplamının
+    kendisine eşit olup olmadığını test eden isArmstrong isimli fonksiyonu yazınız ve aşağıdaki kod ile test ediniz.
+    Fonksiyon negatif değerler için false değerini döndürecektir.
+
+    Açıklama: Kuvvet alma işlemi için bir önceki örnekte yazılan pow fonksiyonu kullanılacaktır.
+---------------------------------------------------------------------------------------------------------------------*/
+
+fun main() = runIsArmstongTest()
+
+fun runIsArmstongTest() {
+    for(a in -10..99999)
+        if(isArmstrong(a))
+            println(a)
+}
+
+fun isArmstrong(a:Int): Boolean {
+    fun digitsPowSum(): Int {
+        var temp = a
+        val n = countDigits(a)
+        var result = 0;
+
+        while (temp != 0) {
+            result += pow(temp % 10, n)
+            temp /= 10
+        }
+
+        return result
+    }
+    return digitsPowSum() == a
+}
+
+
+// Version 2
+
+fun isArmstrong2(a:Int): Boolean = digitsPowSum2(a) == a
+
+fun digitsPowSum2(a: Int): Int {
+    var temp = a
+    val n = countDigits(a)
+    var result = 0;
+
+    while (temp != 0) {
+        result += pow(temp % 10, n)
+        temp /= 10
+    }
+
+    return result
+}
