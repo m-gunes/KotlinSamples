a9ca2c5a2116407e0ff81b763d9db0d9cd587f2a
m-gunes <mistafagunes@gmail.com>
2024-09-13 09:14:51 +0200

    Create isPrimeSlow.kt

---
+++ src/main/kotlin/org/csystem/coursework/isPrimeSlow.kt
@@ -0,0 +1,28 @@
+package org.csystem.coursework
+
+/*---------------------------------------------------------------------------------------------------------------------
+    Bir sayının asal olup olmadığını test eden isPrime fonksiyonu ve test kodu
+    (Yavaş versiyon)
+    Kural: Bir sayiyi yarisindan sonrasi bolemez!
+---------------------------------------------------------------------------------------------------------------------*/
+
+fun main() = runIsPrimeSlowTest()
+
+fun runIsPrimeSlowTest() {
+    for (n in 1L..100)
+        if (isPrime(n))
+            print("$n ")
+    println()
+    println(if (isPrimeSlow(1_000_003)) "Asal" else "Asal değil")
+}
+
+fun isPrimeSlow(a: Long): Boolean {
+    if(a <= 1)
+        return false
+
+    for(i in 2..a / 2)
+        if(a % i == 0L)
+            return false
+
+    return true
+}
