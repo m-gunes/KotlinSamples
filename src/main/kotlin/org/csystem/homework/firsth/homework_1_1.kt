1b534e09566c9332c8bc0455d7fa73758d8c12ac
m-gunes <mistafagunes@gmail.com>
2024-09-17 14:50:48 +0200

    Update homework_1_1.kt

--- src/main/kotlin/org/csystem/homework/firsth/homework_1_1.kt
+++ src/main/kotlin/org/csystem/homework/firsth/homework_1_1.kt
@@ -1,33 +1,33 @@
+// Homework-001-1.sorunun çözümü
+
 package org.csystem.homework.firsth
 
-import org.csystem.topics.max
+import kotlin.math.max
 import kotlin.math.min
 
-fun main() {
-    print("a?")
-    var a = readln().toInt()
-    print("b?")
-    var b = readln().toInt()
-    print("c?")
-    var c = readln().toInt()
+fun main() = runPrintOrderedNumberApp()
 
+fun runPrintOrderedNumberApp() {
+    println("Üç tane sayı giriniz:")
+    val a = readln().toInt()
+    val b = readln().toInt()
+    val c = readln().toInt()
+
+    printOrderedNumber(a,b,c)
+}
+
+fun printOrderedNumber(a:Int, b:Int, c:Int) {
     val max =  max(a, max(b, c))
     val min = min(a, min(b, c))
     val mid = (a + b + c) - (min + max)
 
-    if(min == mid) {
-        print("$min = $mid ")
-        if(mid == max)
-            print("= $max")
-        else
-            print("< $max")
+    when {
+        min == mid -> print("$min = $mid ")
+        else -> print("$min < $mid ")
     }
 
-    if(min < mid) {
-        print("$min < $mid ")
-        if(mid == max)
-            print("= $max")
-        else
-            print("< $max")
+    when {
+        mid == max -> print("= $max")
+        else -> print("< $max")
     }
 }
