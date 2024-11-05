1d29cf50336fc70ca03f693a185658087a984b5c
m-gunes <mistafagunes@gmail.com>
2024-11-05 12:11:45 +0100

    create printPrimeFactors

---
+++ src/main/kotlin/org/csystem/homework/second/homework_2_3.kt
@@ -0,0 +1,22 @@
+package org.csystem.homework.second
+
+fun main() = runPrimeFactorsTest()
+
+fun runPrimeFactorsTest() {
+    print("Input a number:")
+    val a = readln().toInt()
+    printPrimeFactors(a)
+}
+
+fun printPrimeFactors(a: Int) {
+    var a = a
+    var value = 2
+
+    while (a > 1)
+        if (a % value == 0) {
+            println(value)
+            a /= value
+        }
+        else
+            ++value
+}
