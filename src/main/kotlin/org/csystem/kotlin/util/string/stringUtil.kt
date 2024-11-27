076c4ff7f34144ed9161f6a322158763a05dd5ad
m-gunes <mistafagunes@gmail.com>
2024-11-27 23:15:31 +0100

    Update stringUtil.kt

--- src/main/kotlin/org/csystem/kotlin/util/string/stringUtil.kt
+++ src/main/kotlin/org/csystem/kotlin/util/string/stringUtil.kt
@@ -1,5 +1,20 @@
 package org.csystem.kotlin.util.string
 
+
+fun changeCase(s: String): String {
+    val sb = StringBuilder(s)
+
+    for (i in s.indices)
+        sb[i] = when {
+            sb[i].isUpperCase() -> sb[i].lowercaseChar()
+            else -> sb[i].uppercaseChar()
+        }
+
+    return sb.toString()
+}
+
+fun capitalize(s: String) = if (s != "") s[0].uppercase() + s.substring(1).lowercase() else ""
+
 fun countString(s1: String, s2: String, ignoreCase: Boolean = false): Int {
     var count = 0
     var index = -1
@@ -13,3 +28,14 @@ fun countString(s1: String, s2: String, ignoreCase: Boolean = false): Int {
     }
     return count
 }
+
+fun isPangram(s: String, alphabet: String, ignoreCase: Boolean = false ): Boolean {
+    for (c in alphabet)
+        if (!s.contains(c, ignoreCase))
+            return false
+    return true
+}
+
+fun isPangramTR(s: String) = isPangram(s.lowercase(), "abcçdefgğhıijklmnoöprsştuüvyz")
+
+fun isPangramEN(s: String) = isPangram(s.lowercase(), "abcdefghijklmnopqrstuvwxyz")
